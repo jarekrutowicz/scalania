ba02b02b3d58781a5f5711c79d8b84d3ca7453f0
Jarek Rutowicz <jaroslaw.rutowicz@gmail.com>
2015-10-17 18:57:59 +0200

    removing complex types, they are not necessary

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P11.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P11.scala
@@ -1,5 +1,19 @@
 package pl.japila.scalania.s99
 
 object S99_P11 {
-  def encodeModified[T](ts: Seq[T]): Seq[Either[(Int, T), T]] = ???
+  def encodeModified[T](ts: Seq[T]): Seq[Any] =
+    if (ts.isEmpty)
+      Nil
+    else
+      encodeModifiedAcc(ts.tail, (1, ts.head), Nil).reverse
+
+  def encodeModifiedAcc[T](ts: Seq[T], current: (Int, T), acc: Seq[Any]): Seq[Any] =
+    if (ts.isEmpty)
+      current +: acc
+    else if (ts.head == current._2)
+      encodeModifiedAcc(ts.tail, (current._1 + 1, current._2), acc)
+    else {
+      val newElem : Any = if (current._1 == 1) current._2 else current
+      encodeModifiedAcc(ts.tail, (1, ts.head), newElem +: acc)
+    }
 }
