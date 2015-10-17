bf1113a178e2e99386ab253599fba1016e7ecfe5
Jarek Rutowicz <jaroslaw.rutowicz@gmail.com>
2015-10-17 18:59:44 +0200

    removing complex types from test, they are not necessary

--- exercises/src/test/scala/pl/japila/scalania/s99/P11Spec.scala
+++ exercises/src/test/scala/pl/japila/scalania/s99/P11Spec.scala
@@ -7,7 +7,7 @@ object P11Spec extends Specification {
   "P11 solution" should {
     "Do modified run-length encoding" in {
       val actual = encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-      val expected = List(Left((4, 'a)), Right('b), Left((2, 'c)), Left((2, 'a)), Right('d), Left((4, 'e)))
+      val expected : List[Any] = List((4, 'a), 'b, (2, 'c), (2, 'a), 'd, (4, 'e))
       actual mustEqual expected
     }
     "Applied to an empty list return an empty list" in {
