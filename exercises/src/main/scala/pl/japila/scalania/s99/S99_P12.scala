52613ee5ae5443b3caf55b8d55b5b9d39de7cc84
Jarek Rutowicz <jaroslaw.rutowicz@gmail.com>
2015-10-19 22:13:21 +0200

    solution p12

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P12.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P12.scala
@@ -1,5 +1,21 @@
 package pl.japila.scalania.s99
 
 object S99_P12 {
-  def decode[T](its: Seq[(Int, T)]): Seq[T] = ???
+  def decode[T](its: Seq[(Int, T)]): Seq[T] = {
+    def decodeAcc[T](its: Seq[(Int, T)], acc: Seq[T]): Seq[T] =
+      if (its.isEmpty)
+        acc
+      else {
+        if (its.head._1 == 1)
+          decodeAcc(its.tail, its.head._2+:acc)
+        else
+          decodeAcc((its.head._1-1, its.head._2)+:its.tail, its.head._2+:acc)
+      }
+
+      decodeAcc(its, Nil).reverse
+  }
+
+  object Solution2 { // jacek laskowski
+    def decode[T](its: Seq[(Int, T)]): Seq[T] = its.flatMap(e => List.fill(e._1)(e._2))
+  }
 }
